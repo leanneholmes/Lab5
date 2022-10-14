d84fcab741c728242a86eb138b95e8a8c962a6e8
Man Sun <msun45@my.bcit.ca>
2022-10-13 19:49:01 -0700

    add two fragments and navigation buttons

---
+++ app/src/main/java/com/example/lab5/BudgetFragment.java
@@ -0,0 +1,9 @@
+package com.example.lab5;
+
+import androidx.fragment.app.Fragment;
+
+public class BudgetFragment extends Fragment {
+    public BudgetFragment() {
+        super(R.layout.fragment_budget);
+    }
+}
